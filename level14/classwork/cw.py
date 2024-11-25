7fea8e7423f849ccbe2fd493622557cbfb096191
giokacitadzeee <gukanemsadze1990@gmail.com>
2024-11-26 01:25:47 +0400

    second commit

---
+++ level14/classwork/cw.py
@@ -0,0 +1,18 @@
+#1) გამოიყენეთ ორივე ლოგიკური ოპერატორი and და or და გააკეთეთ ყველა შესაძლო ვარიანტი 
+# ლოგიკური წინადადებების, and და or ის გამოყენებით გააკეთეთ სხვადასხვა შედარებები 
+# რომ მიიღოთ ბულეან ტიპის მონაცაემები true ან false
+
+
+#declaring variables
+
+age = int(input("how old are you"))
+
+
+#using and and or conditions
+
+if age > 2 and age <= 6:
+    print("youre still in the kindergarten")
+elif age <=2:
+    print("youre not in the kindergarten")
+elif age >= 7:
+    print("youre not in the kindergarten")
