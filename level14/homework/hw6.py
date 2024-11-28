34660893c196a6e822b24e75bb083634a0649cb0
giokacitadzeee <gukanemsadze1990@gmail.com>
2024-11-28 23:24:10 +0400

    second commit

---
+++ level14/homework/hw6.py
@@ -0,0 +1,9 @@
+#შეამოწმეთ შეყვანილი სიტყვა hello-ა თუ hello world.
+word = input("hello or hello world?")
+
+if word == "hello":
+    print("word is hello")
+elif word == "hello world":
+    print("word is hello world")
+else:
+    print("incorrect")
